95008545c835cb2fffb1c330800a11f1b159f53c
Alen Calakovic <calakovicalen@gmail.com>
2023-12-22 08:37:28 +0100

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var logo_svg_1 = __importDefault(require("./logo.svg"));
+require("./App.css");
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (React.createElement("div", { className: "App" },
+        React.createElement("header", { className: "App-header" },
+            React.createElement("img", { src: logo_svg_1.default, className: "App-logo", alt: "logo" }),
+            React.createElement("p", null,
+                "Edit ",
+                React.createElement("code", null, "src/App.js"),
+                " and save to reload."),
+            React.createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React"))));
 }
-
-export default App;
+exports.default = App;
